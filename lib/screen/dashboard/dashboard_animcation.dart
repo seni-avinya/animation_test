df791a13e04b9863d5bf4d2e6e961d7d3ffe32a7
seni-avinya <senipatel.avinya@gmail.com>
2025-12-02 14:22:25 +0530

    first commit

---
+++ lib/screen/dashboard/dashboard_animcation.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class DashboardAnimcation extends StatefulWidget {
+  const DashboardAnimcation({super.key});
+
+  @override
+  _DashboardAnimcationState createState() => _DashboardAnimcationState();
+}
+
+class _DashboardAnimcationState extends State<DashboardAnimcation> {
+  bool _big = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: 800),
+          curve: Curves.elasticOut,
+          width: _big ? 300 : 100,
+          height: _big ? 300 : 100,
+          color: _big ? Colors.red : Colors.blue,
+          child: FlutterLogo(),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() => _big = !_big),
+        child: Icon(Icons.play_arrow),
+      ),
+    );
+  }
+}
